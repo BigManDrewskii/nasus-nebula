f1143a28111b1fcf1feabbb1d55826eec17fcbe3
BigManDrewskii <drewskiiart@gmail.com>
2026-03-07 18:33:49 +0200

    chore: add MIT license and unhandled rejection handler

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { warmEmbeddingModel } from './agent/memory/transformersEmbedding'
+import { logger } from './lib/logger'
+
+window.addEventListener('unhandledrejection', (event) => {
+  logger.error(
+    'UnhandledRejection',
+    'An unhandled promise rejection occurred.',
+    event.reason
+  )
+})
 
 // Kick off model download in background so first memory search is fast
 warmEmbeddingModel()
